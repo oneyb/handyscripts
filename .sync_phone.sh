4b36e5b8f7f10c07d257d91583d37454eea21a7b
oneyb <zenlines@gmail.com>
2017-01-10 23:10:30 +0100

    saving things

---
+++ .sync_phone.sh
@@ -0,0 +1,88 @@
+#!/bin/bash -x
+
+docs=~/documents
+
+adb start-server
+adb wait-for-device
+
+if [ "`adb devices | sed -r '/ice$/!d;s/^([[:alnum:]]+).*device$/\1/'`" == "4200f424d44d5200" ];
+then
+    echo syncing phone
+    phone=1
+    USB=rsync://localhost:6010/root/storage/extSdCard
+    USB1=rsync://localhost:6010/root/storage/sdcard0
+else
+    echo syncing tablet
+    phone=0
+    USB=rsync://localhost:6010/root/storage/2601-20A9
+    USB1=rsync://localhost:6010/root/storage/sdcard
+fi
+
+if [[ ! -f ~/phone/rsync.bin ]]; then
+    wget -O ~/phone/rsync.bin http://github.com/pts/rsyncbin/raw/master/rsync.rsync4android
+fi
+
+adb push ~/phone/rsync.bin /data/local/tmp/rsync
+adb shell chmod 755 /data/local/tmp/rsync
+
+adb shell 'exec >/sdcard/rsyncd.conf && echo address = 127.0.0.1 && echo port = 1873 && echo "[root]" && echo path = / && echo use chroot = false && echo read only = false'
+# adb shell '/data/local/tmp/rsync --daemon --config=/sdcard/rsyncd.conf --log-file=/data/local/tmp/foo &'
+
+adb shell /data/local/tmp/rsync --daemon --no-detach --config=/sdcard/rsyncd.conf --log-file=/proc/self/fd/2 &
+adb forward tcp:6010 tcp:1873
+
+echo Syncing...
+sleep 1
+
+if [[ $# -eq 0 ]] | [[ $1 != "in" ]]; then
+    # Stuff to sync
+    stuff="org-emacs jobsearch config pubmaterials marriage training_tourenleiter"
+    for s in $stuff; do
+        rsync -vrulDO --size-only --delete $docs/$s/ $USB/$s/
+    done
+
+    # Action, Books
+    stuff="action books vipassana Breeding"
+    for s in $stuff; do
+        rsync -vrulDOL --size-only --delete ~/$s/ $USB/$s/
+    done
+
+    # Musica!
+    if [[ $phone -eq 1 ]]; then
+        rsync -vrulDO --size-only --delete /d/music/essence/ $USB/music/
+    else
+        rsync -vrulDO --size-only --delete /d/music/faves/ $USB/music/faves/
+    fi
+
+    # Pix
+    rsync -vrulDO --size-only --delete $USB1/DCIM/Camera/* $HOME/pictures/phone/
+    if [[ $phone -eq 1 ]]; then
+        rsync -vrulDO --size-only $USB1/Contact* $HOME/documents/contacts/
+    fi
+
+else
+
+    stuff="org-emacs jobsearch pubmaterials marriage training_tourenleiter"
+    for s in $stuff; do
+        rsync -vurt --delete $USB/$s/ $docs/$s/
+    done
+
+    # Action, Books
+    stuff="action books vipassana Breeding"
+    for s in $stuff; do
+        rsync -vurt --delete $USB/$s/ ~/$s/
+    done
+
+fi
+
+# Pix
+rsync -vrulDO --size-only --delete $USB1/DCIM/Camera/* $HOME/pictures/phone/
+if [[ $phone -eq 1 ]]; then
+    rsync -vrulDO --size-only $USB1/Contact* $HOME/documents/contacts/
+fi
+
+adb forward --remove tcp:6010
+adb shell rm -f /sdcard/rsyncd.conf
+adb shell rm -f /data/local/tmp/rsync
+adb kill-server
+echo Phone can now be removed
--- README.md
+++
@@ -1,2 +0,0 @@
-# handyscripts
-repository for scripts that have stood the test of time and optimization
